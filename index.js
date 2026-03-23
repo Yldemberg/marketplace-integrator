ed3ac8121d768a2dcd10ad1095680f66aaf5601e
yldemberg oliveira <yldemberg@gmail.com>
2026-03-23 20:46:05 -0300

    fix: adicionando healthcheck

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ app.post('/webhook', (req, res) => {
   console.log(req.body);
   res.send('ok');
 });
-
+app.get('/api/healthz', (req, res) => {
+  res.send('ok');
+});
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log('Servidor rodando na porta ' + PORT);
